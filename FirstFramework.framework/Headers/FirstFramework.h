37d8b4555001eb54383de228f68865e4a6a47b2b
zhengsheng <zhengsheng120@126.com>
2020-02-21 15:38:57 +0800

    first

---
+++ FirstFramework.framework/Headers/FirstFramework.h
@@ -0,0 +1,19 @@
+//
+//  FirstFramework.h
+//  FirstFramework
+//
+//  Created by zhengsheng on 2020/2/19.
+//  Copyright © 2020 zhengsheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstFramework.
+FOUNDATION_EXPORT double FirstFrameworkVersionNumber;
+
+//! Project version string for FirstFramework.
+FOUNDATION_EXPORT const unsigned char FirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFramework/PublicHeader.h>
+
+#import <SayHi.h>
---
+++ FirstFramework.framework/Headers/SayHi.h
@@ -0,0 +1,19 @@
+//
+//  SayHi.h
+//  FirstFramework
+//
+//  Created by zhengsheng on 2020/2/19.
+//  Copyright © 2020 zhengsheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SayHi : NSObject
+
+- (void)sayHi;
+
+@end
+
+NS_ASSUME_NONNULL_END
